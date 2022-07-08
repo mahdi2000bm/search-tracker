912f37d59e96dd67345b28e391e095ebc83ef61c
mhdibaqri <mahdi2000bm@gmail.com>
2022-07-08 16:19:36 +0430

    final front search box

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -1,40 +1,30 @@
+function includes(result_row, key) {
+    for (let i = 0; i < result_row.length; i++)
+        if (result_row[i] === key)
+            return true;
+    return false
+}
+let jq = jQuery
+
 let search_form_input = document.getElementsByClassName('elementor-search-form__input')[0]
     search_form_input.setAttribute("autocomplete", "off")
     
 let search_form_contain = document.getElementsByClassName('elementor-search-form__container')[0]
+let boxsearch = document.getElementsByClassName('box-search')[0]
 
-let boxsearch = jQuery('.box-search')[0]
-let resultRow = jQuery('.result-row');    
-let recentlyTitle = jQuery('.recently-title');
-let recentlyResult = jQuery('.recently-result');
+    jq(search_form_input).click(function(){
+        jq(this).addClass('search-box-active')
+        jq(boxsearch).addClass("box-search-active")
+    })
+    
+    jq(window).click(function(event) {
 
-if (e.target == boxsearch || includes(resultRow,e.target) || includes(recentlyResult,e.target) || includes(recentlyTitle,e.target) || e.target == boxClicked) {
-    jQuery(searchInput).addClass('search-box-active')
-    jQuery('.box-search').addClass("box-search-active");
-} else {
-    jQuery(searchInput).removeClass('search-box-active');
-    jQuery('.box-search').removeClass("box-search-active");
-}
-
-
-
-
-jQuery('.elementor-search-form__input').on('change', function() {
-    if (this.value.length > 2) {
-        jQuery.ajax({
-            type: "post",
-            // It must changed to /wp-admin/admin-ajax.php
-            url: "http://localhost/wordpress/wp-admin/admin-ajax.php",
-            data: {
-                action: 'searchFormText',
-                data: this.value
-            },
-            dataType: "json",
-            success: function(response) {
-                console.log(response)
-            },
-            error: function(response) {
-            }
-        });
-    };
-});
+        if (event.target == boxsearch || event.target == search_form_input) {
+            jQuery(search_form_contain).addClass('search-box-active')
+            jQuery(boxsearch).addClass("box-search-active");
+            
+        } else {
+            jQuery(search_form_contain).removeClass('search-box-active');
+            jQuery(boxsearch).removeClass("box-search-active");
+        }
+    })
